455b6e1e8ce1fa925e31c8546c640c4d4b6bf3d1
Omar Flores <sazardev@gmail.com>
2024-06-24 16:38:11 -0700

    refactor: Update drawerbar to use appRoutes for navigation

--- lib/components/drawerbar.dart
+++ lib/components/drawerbar.dart
@@ -2,38 +2,72 @@ import 'package:clean_stock/components/drawerbutton.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-const routes = [
-  {"Home": Icons.home},
-  {"Products": Icons.shopping_bag},
-  {"Categories": Icons.category},
-  {"Orders": Icons.shopping_cart},
-  {"Users": Icons.person},
-  {"Settings": Icons.settings},
+const appRoutes = [
+  {
+    "title": "Home",
+    "icon": Icons.home,
+    "secondaryIcon": Icons.arrow_forward_ios,
+    "route": "/",
+  },
+  {
+    "title": "Products",
+    "icon": Icons.shopping_bag,
+    "secondaryIcon": Icons.arrow_forward_ios,
+    "route": "/products",
+  },
+  {
+    "title": "Categories",
+    "icon": Icons.category,
+    "secondaryIcon": Icons.arrow_forward_ios,
+    "route": "/categories",
+  },
+  {
+    "title": "Orders",
+    "icon": Icons.shopping_cart,
+    "secondaryIcon": Icons.arrow_forward_ios,
+    "route": "/orders",
+  },
+  {
+    "title": "Users",
+    "icon": Icons.person,
+    "secondaryIcon": Icons.arrow_forward_ios,
+    "route": "/users",
+  },
+  {
+    "title": "Settings",
+    "icon": Icons.settings,
+    "secondaryIcon": Icons.arrow_forward_ios,
+    "route": "/settings",
+  }
 ];
 
-class Drawerbar extends ConsumerWidget {
+class Drawerbar extends StatefulWidget {
   const Drawerbar({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  State<Drawerbar> createState() => _DrawerbarState();
+}
+
+class _DrawerbarState extends State<Drawerbar> {
+  final index = 0;
+
+  @override
+  Widget build(BuildContext context) {
     return SizedBox(
       width: 200,
       child: Drawer(
         shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
         child: ListView(
-          children: [
-            ...routes.asMap().entries.map((e) {
-              final index = e.key;
-              final route = e.value.keys.first;
-              final icon = e.value.values.first;
-
-              return Drawerbutton(
-                  title: route,
-                  icon: icon,
-                  isSelected: index == 0,
-                  onTap: () {});
-            })
-          ],
+          children: appRoutes.asMap().entries.map((entry) {
+            final routeInfo = entry.value; // Access the map via value
+            return Drawerbutton(
+              title: routeInfo['title'] as String,
+              icon: routeInfo['icon'] as IconData,
+              secondaryIcon: routeInfo['secondaryIcon'] as IconData,
+              isSelected: currentRoute == routeInfo['route'],
+              onTap: () {},
+            );
+          }).toList(),
         ),
       ),
     );
--- lib/core/router/app.router.dart
+++ lib/core/router/app.router.dart
@@ -1,10 +1,13 @@
 import 'package:clean_stock/layouts/home.layout.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-    path: '/',
-    name: 'home',
-    builder: (context, route) => const HomeLayout(),
-  )
-]);
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      name: 'home',
+      builder: (context, route) => const HomeLayout(),
+    )
+  ],
+);
