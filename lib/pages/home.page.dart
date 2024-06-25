7be1ef2bb125b2eb6141d073f5bcb90aeb5598f5
Omar Flores <sazardev@gmail.com>
2024-06-25 14:53:01 -0700

    refactor: Update HomePage to use ConsumerWidget for state management

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,11 +1,12 @@
 import 'package:clean_stock/layouts/home.layout.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(context, ref) {
     return const HomeLayout(
       children: Center(
         child: Text('Home'),
