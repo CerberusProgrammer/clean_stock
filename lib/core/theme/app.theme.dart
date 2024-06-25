4bcc9cb24cf3484156d1dd1c9bcbbedc78127198
Omar Flores <sazardev@gmail.com>
2024-06-24 17:29:04 -0700

    refactor: Update app theme to use Google Fonts for text styling

--- lib/core/theme/app.theme.dart
+++ lib/core/theme/app.theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart'; // Importa google_fonts
 
 final colorProvider = StateProvider<Color>((ref) {
   return Colors.indigo;
@@ -9,5 +10,8 @@ final themeProvider = Provider<ThemeData>(
   (ref) => ThemeData(
     colorSchemeSeed: ref.watch(colorProvider),
     brightness: Brightness.light,
+    textTheme: GoogleFonts.manropeTextTheme(
+      ThemeData(brightness: Brightness.light).textTheme,
+    ),
   ),
 );
