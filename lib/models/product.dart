455cad3d38e0e2397f7a259e54622384bf763db2
Omar Flores <sazardev@gmail.com>
2024-06-25 14:10:19 -0700

    refactor: Update Product model serialization methods

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -49,31 +49,61 @@ class Product {
     required this.updatedAt,
   });
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'description': description,
-      'barcode': barcode,
-      'weight': weight,
-      'dimension': dimension,
-      'expiration_date': expirationDate?.toIso8601String(),
-      'location': location,
-      'manufacturer': manufacturer?.toJson(),
-      'supplier': supplier?.toJson(),
-      'icon': icon,
-      'image': image,
-      'status': status,
-      'price': price,
-      'quantity': quantity,
-      'quantity_min': quantityMin,
-      'quantity_max': quantityMax,
-      'color': color,
-      'category': category?.toJson(),
-      'created_at': createdAt.toIso8601String(),
-      'updated_at': updatedAt.toIso8601String(),
-    };
-  }
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+        id: json['id'],
+        name: json['name'],
+        description: json['description'],
+        barcode: json['barcode'],
+        weight: json['weight'],
+        dimension: json['dimension'],
+        expirationDate: json['expiration_date'] != null
+            ? DateTime.parse(json['expiration_date'])
+            : null,
+        location: json['location'],
+        manufacturer: json['manufacturer'] != null
+            ? Manufacturer.fromJson(json['manufacturer'])
+            : null,
+        supplier: json['supplier'] != null
+            ? Supplier.fromJson(json['supplier'])
+            : null,
+        icon: json['icon'],
+        image: json['image'],
+        status: json['status'],
+        price: json['price'],
+        quantity: json['quantity'],
+        quantityMin: json['quantity_min'],
+        quantityMax: json['quantity_max'],
+        color: json['color'],
+        category: json['category'] != null
+            ? Category.fromJson(json['category'])
+            : null,
+        createdAt: DateTime.parse(json['created_at']),
+        updatedAt: DateTime.parse(json['updated_at']),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'description': description,
+        'barcode': barcode,
+        'weight': weight,
+        'dimension': dimension,
+        'expiration_date': expirationDate?.toIso8601String(),
+        'location': location,
+        'manufacturer': manufacturer?.toJson(),
+        'supplier': supplier?.toJson(),
+        'icon': icon,
+        'image': image,
+        'status': status,
+        'price': price,
+        'quantity': quantity,
+        'quantity_min': quantityMin,
+        'quantity_max': quantityMax,
+        'color': color,
+        'category': category?.toJson(),
+        'created_at': createdAt.toIso8601String(),
+        'updated_at': updatedAt.toIso8601String(),
+      };
 
   @override
   String toString() {
