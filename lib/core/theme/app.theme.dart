305ec26c4c8e82a79b5ae424c2022fe22a3cf32f
Omar Flores <sazardev@gmail.com>
2024-06-25 12:57:50 -0700

    refactor: Update app.theme.dart to remove unnecessary import statement

--- lib/core/theme/app.theme.dart
+++ lib/core/theme/app.theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart'; // Importa google_fonts
+import 'package:google_fonts/google_fonts.dart';
 
 final colorProvider = StateProvider<Color>((ref) {
   return Colors.indigo;
