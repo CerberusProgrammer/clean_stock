3ae0ee603a06e9224b1b7ed8049d5fd551c6471c
Omar Flores <sazardev@gmail.com>
2024-06-25 12:18:08 -0700

    feat: Add dark mode switch to SettingsPage

--- lib/pages/settings.page.dart
+++ lib/pages/settings.page.dart
@@ -1,14 +1,30 @@
+import 'package:clean_stock/core/theme/app.theme.dart';
 import 'package:clean_stock/layouts/home.layout.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends ConsumerWidget {
   const SettingsPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const HomeLayout(
-      children: Center(
-        child: Text('Settings'),
+  Widget build(context, ref) {
+    return HomeLayout(
+      children: Column(
+        children: [
+          FilledButton(
+            onPressed: () => context.go('/settings/profile'),
+            child: const Text('Perfil'),
+          ),
+          SwitchListTile(
+            title: const Text('Tema oscuro'),
+            value: ref.watch(brighnessProvider) != Brightness.light,
+            onChanged: (value) {
+              ref.read(brighnessProvider.notifier).state =
+                  value ? Brightness.dark : Brightness.light;
+            },
+          )
+        ],
       ),
     );
   }
