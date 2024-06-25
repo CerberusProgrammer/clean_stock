96f620c9248b55feeae53b208e65b55a942999d7
Omar Flores <sazardev@gmail.com>
2024-06-25 14:10:31 -0700

    refactor: Simplify Supplier.fromJson and Supplier.toJson methods

--- lib/models/supplier.dart
+++ lib/models/supplier.dart
@@ -31,43 +31,39 @@ class Supplier {
     required this.updatedAt,
   });
 
-  factory Supplier.fromJson(Map<String, dynamic> json) {
-    return Supplier(
-      id: json['id'],
-      name: json['name'],
-      icon: json['icon'],
-      description: json['description'],
-      address: json['address'],
-      website: json['website'],
-      contactEmail: json['contact_email'],
-      contactPhone: json['contact_phone'],
-      country: json['country'],
-      city: json['city'],
-      status: json['status'],
-      color: json['color'],
-      createdAt: DateTime.parse(json['created_at']),
-      updatedAt: DateTime.parse(json['updated_at']),
-    );
-  }
+  factory Supplier.fromJson(Map<String, dynamic> json) => Supplier(
+        id: json['id'],
+        name: json['name'],
+        icon: json['icon'],
+        description: json['description'],
+        address: json['address'],
+        website: json['website'],
+        contactEmail: json['contact_email'],
+        contactPhone: json['contact_phone'],
+        country: json['country'],
+        city: json['city'],
+        status: json['status'],
+        color: json['color'],
+        createdAt: DateTime.parse(json['created_at']),
+        updatedAt: DateTime.parse(json['updated_at']),
+      );
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'icon': icon,
-      'description': description,
-      'address': address,
-      'website': website,
-      'contact_email': contactEmail,
-      'contact_phone': contactPhone,
-      'country': country,
-      'city': city,
-      'status': status,
-      'color': color,
-      'created_at': createdAt.toIso8601String(),
-      'updated_at': updatedAt.toIso8601String(),
-    };
-  }
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'icon': icon,
+        'description': description,
+        'address': address,
+        'website': website,
+        'contact_email': contactEmail,
+        'contact_phone': contactPhone,
+        'country': country,
+        'city': city,
+        'status': status,
+        'color': color,
+        'created_at': createdAt.toIso8601String(),
+        'updated_at': updatedAt.toIso8601String(),
+      };
 
   @override
   String toString() {
