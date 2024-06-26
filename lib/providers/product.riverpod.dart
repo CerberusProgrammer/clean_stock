25da8b2b9099dfb7b48bcad9b1aa2f3f789a08bc
Omar Flores <sazardev@gmail.com>
2024-06-26 08:06:51 -0700

    feat: Add product providers for managing products

---
+++ lib/providers/product.riverpod.dart
@@ -0,0 +1,38 @@
+import 'package:clean_stock/models/product.dart';
+import 'package:clean_stock/providers/user.riverpod.dart';
+import 'package:clean_stock/services/product.service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final productsProvider = FutureProvider<List<Product>>(
+  (ref) async => ProductService.getProducts(
+    token: ref.watch(userProvider).token,
+  ),
+);
+
+final productProvider = FutureProvider.family<Product, String>(
+  (ref, id) async => ProductService.getProduct(
+    id: id,
+    token: ref.watch(userProvider).token,
+  ),
+);
+
+final createProductProvider = FutureProvider.family<Product, Product>(
+  (ref, product) async => ProductService.createProduct(
+    product: product,
+    token: ref.watch(userProvider).token,
+  ),
+);
+
+final updateProductProvider = FutureProvider.family<Product, Product>(
+  (ref, product) async => ProductService.updateProduct(
+    product: product,
+    token: ref.watch(userProvider).token,
+  ),
+);
+
+final deleteProductProvider = FutureProvider.family<void, String>(
+  (ref, id) async => ProductService.deleteProduct(
+    id: id,
+    token: ref.watch(userProvider).token,
+  ),
+);
