0e507fa4b0c50b08541406ccb2097719864ea4fa
Omar Flores <sazardev@gmail.com>
2024-06-26 08:06:41 -0700

    refactor: Add ProductService class for managing products

---
+++ lib/services/product.service.dart
@@ -0,0 +1,33 @@
+import 'package:clean_stock/api/product.api.dart';
+import 'package:clean_stock/models/product.dart';
+
+class ProductService {
+  static Future<List<Product>> getProducts({
+    required String token,
+  }) async =>
+      await ProductAPI.getProducts(token: token);
+
+  static Future<Product> getProduct({
+    required String id,
+    required String token,
+  }) async =>
+      await ProductAPI.getProduct(id: id, token: token);
+
+  static Future<Product> createProduct({
+    required Product product,
+    required String token,
+  }) async =>
+      await ProductAPI.createProduct(product: product, token: token);
+
+  static Future<Product> updateProduct({
+    required Product product,
+    required String token,
+  }) async =>
+      await ProductAPI.updateProduct(product: product, token: token);
+
+  static Future<void> deleteProduct({
+    required String id,
+    required String token,
+  }) async =>
+      await ProductAPI.deleteProduct(id: id, token: token);
+}
