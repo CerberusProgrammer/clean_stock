8610b8aa16a96ca530baacfdea70f96153455a9f
Omar Flores <sazardev@gmail.com>
2024-06-26 08:01:43 -0700

    refactor: Update PromotionService methods to use arrow function syntax

--- lib/services/promotion.service.dart
+++ lib/services/promotion.service.dart
@@ -2,37 +2,32 @@ import 'package:clean_stock/api/promotion.api.dart';
 import 'package:clean_stock/models/promotion.dart';
 
 class PromotionService {
-  static Future<List<Promotion>> getPromotions({required String token}) async {
-    return await PromotionAPI.getPromotions(token: token);
-  }
+  static Future<List<Promotion>> getPromotions({
+    required String token,
+  }) async =>
+      await PromotionAPI.getPromotions(token: token);
 
   static Future<Promotion> getPromotion({
     required String id,
     required String token,
-  }) async {
-    return await PromotionAPI.getPromotion(id: id, token: token);
-  }
+  }) async =>
+      await PromotionAPI.getPromotion(id: id, token: token);
 
   static Future<Promotion> createPromotion({
     required Promotion promotion,
     required String token,
-  }) async {
-    return await PromotionAPI.createPromotion(
-        promotion: promotion, token: token);
-  }
+  }) async =>
+      await PromotionAPI.createPromotion(promotion: promotion, token: token);
 
   static Future<Promotion> updatePromotion({
     required Promotion promotion,
     required String token,
-  }) async {
-    return await PromotionAPI.updatePromotion(
-        promotion: promotion, token: token);
-  }
+  }) async =>
+      await PromotionAPI.updatePromotion(promotion: promotion, token: token);
 
   static Future<void> deletePromotion({
     required String id,
     required String token,
-  }) async {
-    return await PromotionAPI.deletePromotion(id: id, token: token);
-  }
+  }) async =>
+      await PromotionAPI.deletePromotion(id: id, token: token);
 }
