7d7517318c680b30fc5542f5f8a6d16dbde48bec
Omar Flores <sazardev@gmail.com>
2024-06-26 08:01:51 -0700

    refactor: Update promotionsProvider and promotionProvider to use user token

--- lib/providers/promotions.riverpod.dart
+++ lib/providers/promotions.riverpod.dart
@@ -1,27 +1,38 @@
 import 'package:clean_stock/models/promotion.dart';
+import 'package:clean_stock/providers/user.riverpod.dart';
 import 'package:clean_stock/services/promotion.service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final promotionsProvider = FutureProvider<List<Promotion>>((ref) async {
-  return PromotionService.getPromotions(token: '');
-});
+final promotionsProvider = FutureProvider<List<Promotion>>(
+  (ref) async => PromotionService.getPromotions(
+    token: ref.watch(userProvider).token,
+  ),
+);
 
-final promotionProvider =
-    FutureProvider.family<Promotion, String>((ref, id) async {
-  return PromotionService.getPromotion(id: id, token: '');
-});
+final promotionProvider = FutureProvider.family<Promotion, String>(
+  (ref, id) async => PromotionService.getPromotion(
+    id: id,
+    token: ref.watch(userProvider).token,
+  ),
+);
 
-final createPromotionProvider =
-    FutureProvider.family<Promotion, Promotion>((ref, promotion) async {
-  return PromotionService.createPromotion(promotion: promotion, token: '');
-});
+final createPromotionProvider = FutureProvider.family<Promotion, Promotion>(
+  (ref, promotion) async => PromotionService.createPromotion(
+    promotion: promotion,
+    token: ref.watch(userProvider).token,
+  ),
+);
 
-final updatePromotionProvider =
-    FutureProvider.family<Promotion, Promotion>((ref, promotion) async {
-  return PromotionService.updatePromotion(promotion: promotion, token: '');
-});
+final updatePromotionProvider = FutureProvider.family<Promotion, Promotion>(
+  (ref, promotion) async => PromotionService.updatePromotion(
+    promotion: promotion,
+    token: ref.watch(userProvider).token,
+  ),
+);
 
-final deletePromotionProvider =
-    FutureProvider.family<void, String>((ref, id) async {
-  return PromotionService.deletePromotion(id: id, token: '');
-});
+final deletePromotionProvider = FutureProvider.family<void, String>(
+  (ref, id) async => PromotionService.deletePromotion(
+    id: id,
+    token: ref.watch(userProvider).token,
+  ),
+);
