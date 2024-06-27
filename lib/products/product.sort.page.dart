d96f4448a3fb4b50d165a4d2650c5958c87c2036
Omar Flores <sazardev@gmail.com>
2024-06-27 12:57:44 -0700

    refactor: Update ProductSortPage to dynamically generate category list based on provided categories

--- lib/products/product.sort.page.dart
+++ lib/products/product.sort.page.dart
@@ -1,40 +1,37 @@
 import 'package:clean_stock/components/selectable_listtile.dart';
+import 'package:clean_stock/models/category.dart';
 import 'package:flutter/material.dart';
 
 class ProductSortPage extends StatelessWidget {
   const ProductSortPage({
     super.key,
+    required this.categories,
   });
+
+  final List<Category> categories;
 
   @override
   Widget build(BuildContext context) {
+    print(categories);
+
     return ListView(
       children: [
         ExpansionTile(
-          title: const Text('Categories'),
-          children: [
-            SelectableListTile(
-              title: 'Category 1',
-              icon: Icons.category,
-              onTap: (bool isTapped) {},
-            ),
-            SelectableListTile(
-              title: 'Category 2',
-              icon: Icons.category,
-              onTap: (bool isTapped) {},
-            ),
-          ],
-        ),
+            title: const Text('Categories'),
+            children: categories
+                .map(
+                  (category) => SelectableListTile(
+                    title: category.name,
+                    icon: Icons.category,
+                    onTap: (bool isTapped) {},
+                  ),
+                )
+                .toList()),
         ExpansionTile(
           title: const Text('Manufacturers'),
           children: [
             SelectableListTile(
               title: 'Manufacturer 1',
-              icon: Icons.business,
-              onTap: (bool isTapped) {},
-            ),
-            SelectableListTile(
-              title: 'Manufacturer 2',
               icon: Icons.business,
               onTap: (bool isTapped) {},
             ),
@@ -45,11 +42,6 @@ class ProductSortPage extends StatelessWidget {
           children: [
             SelectableListTile(
               title: 'Supplier 1',
-              icon: Icons.supervisor_account,
-              onTap: (bool isTapped) {},
-            ),
-            SelectableListTile(
-              title: 'Supplier 2',
               icon: Icons.supervisor_account,
               onTap: (bool isTapped) {},
             ),
