1ca2b61f86b03cea462bfab667122d230f50204c
Omar Flores <sazardev@gmail.com>
2024-06-27 13:43:09 -0700

    refactor: Update ProductSortPage to dynamically generate category, manufacturer, and supplier lists

--- lib/products/product.sort.page.dart
+++ lib/products/product.sort.page.dart
@@ -1,52 +1,65 @@
 import 'package:clean_stock/components/selectable_listtile.dart';
-import 'package:clean_stock/models/category.dart';
+import 'package:clean_stock/models/Category.dart';
+import 'package:clean_stock/models/manufacturer.dart';
+import 'package:clean_stock/models/supplier.dart';
 import 'package:flutter/material.dart';
 
 class ProductSortPage extends StatelessWidget {
   const ProductSortPage({
     super.key,
     required this.categories,
+    required this.manufacturers,
+    required this.suppliers,
   });
 
-  final List<Category> categories;
+  final List<Category?> categories;
+  final List<Manufacturer?> manufacturers;
+  final List<Supplier?> suppliers;
 
   @override
   Widget build(BuildContext context) {
-    print(categories);
-
     return ListView(
       children: [
         ExpansionTile(
-            title: const Text('Categories'),
-            children: categories
+          title: const Text('Categories'),
+          children: categories
+              .map(
+                (category) => category != null
+                    ? SelectableListTile(
+                        title: category.name,
+                        icon: Icons.category,
+                        onTap: (bool isTapped) {},
+                      )
+                    : const SizedBox(),
+              )
+              .toList(),
+        ),
+        ExpansionTile(
+            title: const Text('Manufacturers'),
+            children: manufacturers
                 .map(
-                  (category) => SelectableListTile(
-                    title: category.name,
-                    icon: Icons.category,
-                    onTap: (bool isTapped) {},
-                  ),
+                  (manufacturer) => manufacturer != null
+                      ? SelectableListTile(
+                          title: manufacturer.name,
+                          icon: Icons.business,
+                          onTap: (bool isTapped) {},
+                        )
+                      : const SizedBox(),
                 )
                 .toList()),
         ExpansionTile(
-          title: const Text('Manufacturers'),
-          children: [
-            SelectableListTile(
-              title: 'Manufacturer 1',
-              icon: Icons.business,
-              onTap: (bool isTapped) {},
-            ),
-          ],
-        ),
-        ExpansionTile(
-          title: const Text('Suppliers'),
-          children: [
-            SelectableListTile(
-              title: 'Supplier 1',
-              icon: Icons.supervisor_account,
-              onTap: (bool isTapped) {},
-            ),
-          ],
-        )
+            title: const Text('Suppliers'),
+            children: suppliers
+                .map(
+                  (supplier) => supplier != null
+                      ? SelectableListTile(
+                          title: supplier.name,
+                          icon: Icons.supervisor_account,
+                          onTap: (bool isTapped) {},
+                        )
+                      : const SizedBox(),
+                )
+                .toList())
       ],
     );
   }
