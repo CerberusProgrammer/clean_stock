6638a622a41de424baaab174685d8d93b52ff3a4
Omar Flores <sazardev@gmail.com>
2024-06-27 07:17:17 -0700

    refactor: Update app router to include product page

--- lib/core/router/app.router.dart
+++ lib/core/router/app.router.dart
@@ -1,32 +1,55 @@
 import 'package:clean_stock/pages/home.page.dart';
 import 'package:clean_stock/pages/profile.page.dart';
 import 'package:clean_stock/pages/settings.page.dart';
+import 'package:clean_stock/products/product.page.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(
-      path: '/',
-      name: 'home',
-      pageBuilder: (context, state) {
-        return const NoTransitionPage(child: HomePage());
-      },
-    ),
+        path: '/',
+        name: 'home',
+        pageBuilder: (context, state) =>
+            const NoTransitionPage(child: HomePage())),
+    GoRoute(
+        path: '/products',
+        name: 'products',
+        pageBuilder: (context, state) =>
+            const NoTransitionPage(child: ProductPage())),
     GoRoute(
         path: '/settings',
         name: 'settings',
-        pageBuilder: (context, state) {
-          return const NoTransitionPage(child: SettingsPage());
-        },
+        pageBuilder: (context, state) =>
+            const NoTransitionPage(child: SettingsPage()),
         routes: [
           GoRoute(
-            path: 'profile',
-            name: 'profile',
-            pageBuilder: (context, state) {
-              return const NoTransitionPage(child: ProfilePage());
-            },
-          )
+              path: 'profile',
+              name: 'profile',
+              pageBuilder: (context, state) =>
+                  const NoTransitionPage(child: ProfilePage()))
         ])
   ],
 );
+
+const appRoutes = [
+  {
+    "title": "Home",
+    "icon": Icons.home,
+    "secondaryIcon": Icons.home_outlined,
+    "route": "/",
+  },
+  {
+    "title": "Products",
+    "icon": Icons.shopping_bag,
+    "secondaryIcon": Icons.shopping_bag_outlined,
+    "route": "/products",
+  },
+  {
+    "title": "Settings",
+    "icon": Icons.settings,
+    "secondaryIcon": Icons.settings_outlined,
+    "route": "/settings",
+  }
+];
