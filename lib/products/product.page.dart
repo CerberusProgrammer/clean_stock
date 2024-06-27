56626bdecb0a17ca9965c00200437b7284e38cb8
Omar Flores <sazardev@gmail.com>
2024-06-27 13:42:22 -0700

    refactor: Add lazy loading for products in ProductPage

--- lib/products/product.page.dart
+++ lib/products/product.page.dart
@@ -1,9 +1,11 @@
 import 'package:clean_stock/components/custom_textbutton.dart';
+import 'package:clean_stock/products/product.riverpod.dart';
 import 'package:clean_stock/products/product.sort.page.dart';
 import 'package:clean_stock/utils/showfade_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:clean_stock/layouts/home.layout.dart';
+import 'package:go_router/go_router.dart';
 
 class ProductPage extends ConsumerStatefulWidget {
   const ProductPage({super.key});
@@ -17,71 +19,88 @@ class _ProductPageState extends ConsumerState<ProductPage> {
 
   @override
   Widget build(BuildContext context) {
+    final products = ref.watch(testProductsProvider);
+
     return HomeLayout(
         floatingButton: FloatingActionButton(
           elevation: 0,
           onPressed: () {},
           child: const Icon(Icons.add),
         ),
-        children: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                TextField(
-                  controller: _controller,
-                  decoration: InputDecoration(
-                    hintText: 'Product or barcode',
-                    prefixIcon: const Icon(Icons.search),
-                    suffix: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.filter,
-                        size: 18,
+        children: products.when(
+            data: (data) {
+              return GestureDetector(
+                onTap: () => FocusScope.of(context).unfocus(),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: _controller,
+                        focusNode: FocusNode(),
+                        decoration: InputDecoration(
+                          hintText: 'Product or barcode',
+                          prefixIcon: const Icon(Icons.search),
+                          suffix: IconButton(
+                            onPressed: () {
+                              _controller.clear();
+                            },
+                            icon: const Icon(
+                              Icons.close,
+                              size: 18,
+                            ),
+                          ),
+                        ),
+                        onChanged: (value) {},
                       ),
-                    ),
-                  ),
-                  onChanged: (value) {},
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: CustomTextButton(
-                        title: 'Order by',
-                        icon: Icons.arrow_downward,
-                        onTap: () => showFadeDialog(
-                          title: 'Order by',
-                          context: context,
-                          child: const Text('asd'),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: CustomTextButton(
+                              title: 'Order by',
+                              icon: Icons.arrow_downward,
+                              onTap: () => showFadeDialog(
+                                title: 'Order by',
+                                context: context,
+                                child: const Text('asd'),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: CustomTextButton(
+                              title: 'Sort by',
+                              icon: Icons.sort,
+                              onTap: () => showFadeDialog(
+                                title: 'Sort by',
+                                context: context,
+                                child: ProductSortPage(
+                                  categories:
+                                      data.map((d) => d.category).toList(),
+                                  manufacturers:
+                                      data.map((d) => d.manufacturer).toList(),
+                                  suppliers:
+                                      data.map((d) => d.supplier).toList(),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+                      ...data.map(
+                        (e) => ListTile(
+                          title: Text(e.name),
+                          leading: CircleAvatar(child: Text(e.name[0])),
+                          subtitle: Text(e.barcode),
+                          trailing: Text(e.price.toString()),
+                          onTap: () => context.go('/products/${e.id}'),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: CustomTextButton(
-                        title: 'Sort by',
-                        icon: Icons.sort,
-                        onTap: () => showFadeDialog(
-                          title: 'Sort by',
-                          context: context,
-                          child: const ProductSortPage(),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 8),
-                ...List.generate(
-                  12,
-                  (index) => ListTile(
-                    leading: CircleAvatar(child: Text('$index')),
-                    title: Text('Product $index'),
-                    trailing: Text('\$$index.00'),
-                    onTap: () {},
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-        ));
+              );
+            },
+            error: (error, track) => Center(child: Text(error.toString())),
+            loading: () => const CircularProgressIndicator()));
   }
 }
