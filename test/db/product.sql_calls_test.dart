0b46d4c9a383878248443f0d4dfb2cc0525a6ae6
Omar Flores <sazardev@gmail.com>
2024-06-28 12:00:08 -0700

    refactor: Add unit tests for ProductSQL class

---
+++ test/db/product.sql_calls_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:clean_stock/products/product.sql.dart';
+import 'package:clean_stock/products/product.dart';
+import 'package:clean_stock/products/product.query.dart';
+
+void main() {
+  group('ProductSQL Tests', () {});
+}
