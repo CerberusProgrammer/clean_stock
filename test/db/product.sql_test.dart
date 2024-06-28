53d16d677b3ef5181555a061d8a7daea67b56fcd
Omar Flores <sazardev@gmail.com>
2024-06-28 12:00:11 -0700

    refactor: Update Product class toJsonForSQL method

--- test/db/product.sql_test.dart
+++ test/db/product.sql_test.dart
@@ -1,59 +1,10 @@
+import 'package:clean_stock/core/domain/db/data_sql.dart';
+import 'package:clean_stock/models/Category.dart';
+import 'package:clean_stock/models/manufacturer.dart';
+import 'package:clean_stock/models/supplier.dart';
 import 'package:clean_stock/products/product.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:path/path.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-
-Future<Database> initializeDB() async {
-  String path = await getDatabasesPath();
-  String dbPath = join(path, 'database.db');
-
-  bool dbExists = await databaseExists(dbPath);
-
-  if (dbExists) {
-    await deleteDatabase(dbPath);
-  }
-
-  return openDatabase(
-    dbPath,
-    onCreate: (database, version) async {
-      await database.execute(
-        """
-        CREATE TABLE products(
-            id INTEGER PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            description TEXT,
-            barcode VARCHAR(255) NOT NULL,
-            weight REAL,
-            dimension VARCHAR(255),
-            expiration_date TEXT,
-            location VARCHAR(255),
-            manufacturer TEXT,
-            supplier TEXT,
-            icon VARCHAR(255),
-            image VARCHAR(255),
-            status INTEGER NOT NULL,
-            price REAL NOT NULL,
-            quantity INTEGER NOT NULL,
-            quantity_min INTEGER,
-            quantity_max INTEGER,
-            color VARCHAR(255),
-            category TEXT,
-            created_at TEXT NOT NULL,
-            updated_at TEXT NOT NULL 
-        )""",
-      );
-    },
-    version: 1,
-  );
-}
-
-Future<void> insertProduct(Database db, Product product) async {
-  await db.insert(
-    'products',
-    product.toJsonForSQL(),
-    conflictAlgorithm: ConflictAlgorithm.replace,
-  );
-}
 
 void main() {
   sqfliteFfiInit();
@@ -73,28 +24,6 @@ void main() {
           [tableName]);
       expect(tables.isNotEmpty, true);
     });
-  });
-
-  test('Insert product using raw SQL', () async {
-    final db = await initializeDB();
-
-    await db.execute("""
-    INSERT INTO products (
-      id, name, description, barcode, weight, dimension, expiration_date,
-      location, manufacturer, supplier, icon, image, status, price,
-      quantity, quantity_min, quantity_max, color, category, created_at, updated_at
-    ) VALUES (
-      9, 'Test Product', 'This is a test product description.', '123456789', 1.0,
-      '10x20x30', '2023-12-31', 'Aisle 4', 'Test Manufacturer', 'Test Supplier',
-      'icon.png', 'image.png', 1, 9.99, 100, 10, 200, 'Red', 'Electronics',
-      '2023-04-01', '2023-04-01'
-    )
-  """);
-
-    final List<Map<String, dynamic>> products = await db.query('products');
-    expect(products.first['name'], 'Test Product');
-
-    await db.close();
   });
 
   test('Insert product using Product object with only required parameters',
@@ -117,7 +46,290 @@ void main() {
       updatedAt: DateTime.now(),
     );
 
-    await insertProduct(db, newProduct);
+    await db.insert(
+      'products',
+      newProduct.toJsonForSQL(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    final List<Map<String, dynamic>> products = await db.query('products');
+    expect(products.length, isNonZero);
+    expect(products.any((p) => p['name'] == newProduct.name), isTrue);
+
+    await db.close();
+  });
+
+  test('Insert product using Product object with a Manufacturer object',
+      () async {
+    final db = await initializeDB();
+
+    var newManufacturer = Manufacturer(
+      id: 1,
+      name: 'Test Manufacturer',
+      icon: 'icon.png',
+      description: 'This is a test manufacturer.',
+      address: '123 Test St.',
+      website: 'www.test.com',
+      contactEmail: ' [email protected]',
+      contactPhone: '123-456-7890',
+      country: 'USA',
+      city: 'Test City',
+      status: true,
+      color: 'blue',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+
+    await db.insert(
+      'manufacturers',
+      newManufacturer.toJsonForSQL(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    var newProduct = Product(
+      id: 6,
+      name: 'Test Product',
+      description: 'This is a test product.',
+      price: 9.99,
+      barcode: 'asd',
+      weight: 1.0,
+      dimension: '10x20x30',
+      expirationDate: DateTime.now(),
+      location: 'Aisle 4',
+      manufacturer: newManufacturer,
+      status: true,
+      quantity: 10,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+
+    await db.insert(
+      'products',
+      newProduct.toJsonForSQL(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    final List<Map<String, dynamic>> products = await db.query('products');
+    expect(products.length, isNonZero);
+    expect(products.any((p) => p['name'] == newProduct.name), isTrue);
+
+    await db.close();
+  });
+
+  test('Insert product using Product object with a Supplier object', () async {
+    final db = await initializeDB();
+
+    var newSupplier = Supplier(
+      id: 1,
+      name: 'Test Supplier',
+      address: '123 Test St.',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+      status: true,
+    );
+
+    await db.insert(
+      'suppliers',
+      newSupplier.toJsonForSQL(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    var newProduct = Product(
+      id: 7,
+      name: 'Test Product',
+      description: 'This is a test product.',
+      price: 9.99,
+      barcode: 'asd',
+      weight: 1.0,
+      dimension: '10x20x30',
+      expirationDate: DateTime.now(),
+      location: 'Aisle 4',
+      supplier: newSupplier,
+      status: true,
+      quantity: 10,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+
+    await db.insert(
+      'products',
+      newProduct.toJsonForSQL(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    final List<Map<String, dynamic>> products = await db.query('products');
+    expect(products.length, isNonZero);
+    expect(products.any((p) => p['name'] == newProduct.name), isTrue);
+
+    await db.close();
+  });
+
+  test('Insert product using Product object with a Category object', () async {
+    final db = await initializeDB();
+
+    var newCategory = Category(
+      id: 1,
+      name: 'Test Category',
+      status: true,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+
+    await db.insert(
+      'categories',
+      newCategory.toJsonForSQL(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    var newProduct = Product(
+      id: 8,
+      name: 'Test Product',
+      description: 'This is a test product.',
+      price: 9.99,
+      barcode: 'asd',
+      weight: 1.0,
+      dimension: '10x20x30',
+      expirationDate: DateTime.now(),
+      location: 'Aisle 4',
+      category: newCategory,
+      status: true,
+      quantity: 10,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+
+    await db.insert(
+      'products',
+      newProduct.toJsonForSQL(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    final List<Map<String, dynamic>> products = await db.query('products');
+    expect(products.length, isNonZero);
+    expect(products.any((p) => p['name'] == newProduct.name), isTrue);
+
+    await db.close();
+  });
+
+  test('Insert product using Product object with all optional parameters',
+      () async {
+    final db = await initializeDB();
+
+    var newProduct = Product(
+      id: 9,
+      name: 'Test Product',
+      description: 'This is a test product.',
+      price: 9.99,
+      barcode: 'asd',
+      weight: 1.0,
+      dimension: '10x20x30',
+      expirationDate: DateTime.now(),
+      location: 'Aisle 4',
+      status: true,
+      quantity: 10,
+      quantityMin: 1,
+      quantityMax: 20,
+      color: 'blue',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+
+    await db.insert(
+      'products',
+      newProduct.toJsonForSQL(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    final List<Map<String, dynamic>> products = await db.query('products');
+    expect(products.length, isNonZero);
+    expect(products.any((p) => p['name'] == newProduct.name), isTrue);
+
+    await db.close();
+  });
+
+  test('Insert product with all optional parameters including objects',
+      () async {
+    final db = await initializeDB();
+
+    var newManufacturer = Manufacturer(
+      id: 2,
+      name: 'Test Manufacturer',
+      icon: 'icon.png',
+      description: 'This is a test manufacturer.',
+      address: '123 Test St.',
+      website: 'www.test.com',
+      contactEmail: ' [email protected]',
+      contactPhone: '123-456-7890',
+      country: 'USA',
+      city: 'Test City',
+      status: true,
+      color: 'blue',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+
+    await db.insert(
+      'manufacturers',
+      newManufacturer.toJsonForSQL(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    var newSupplier = Supplier(
+      id: 2,
+      name: 'Test Supplier',
+      address: '123 Test St.',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+      status: true,
+    );
+
+    await db.insert(
+      'suppliers',
+      newSupplier.toJsonForSQL(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    var newCategory = Category(
+      id: 2,
+      name: 'Test Category',
+      status: true,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+
+    await db.insert(
+      'categories',
+      newCategory.toJsonForSQL(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    var newProduct = Product(
+      id: 10,
+      name: 'Test Product',
+      description: 'This is a test product.',
+      price: 9.99,
+      barcode: 'asd',
+      weight: 1.0,
+      dimension: '10x20x30',
+      expirationDate: DateTime.now(),
+      location: 'Aisle 4',
+      manufacturer: newManufacturer,
+      supplier: newSupplier,
+      category: newCategory,
+      status: true,
+      quantity: 10,
+      quantityMin: 1,
+      quantityMax: 20,
+      color: 'blue',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+
+    await db.insert(
+      'products',
+      newProduct.toJsonForSQL(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
 
     final List<Map<String, dynamic>> products = await db.query('products');
     expect(products.length, isNonZero);
