5e139a9d9f6161faf8cb91858cf2fa29656541ee
sazarcode <cerberusprogrammer@gmail.com>
2024-06-29 03:50:54 -0700

    refactor: Update main.dart to initialize Hive and ensure app directory is available

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,17 @@ import 'package:clean_stock/core/theme/app.theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(const ProviderScope(child: MyApp()));
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory =
+      await path_provider.getApplicationDocumentsDirectory();
+  await Hive.initFlutter(appDocumentDirectory.path);
+
+  runApp(const ProviderScope(child: MyApp()));
+}
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
