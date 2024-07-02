9182e8835922dbf02d99388469e1006594ee9c16
Omar Flores <sazardev@gmail.com>
2024-07-02 08:41:50 -0700

    refactor: Update createProduct method to generate random values for new products

--- lib/products/pages/product.page.dart
+++ lib/products/pages/product.page.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:clean_stock/products/models/product.dart';
 import 'package:clean_stock/products/product.riverpod.dart';
+import 'package:clean_stock/utils/random_string.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:clean_stock/layouts/home.layout.dart';
@@ -12,17 +15,12 @@ class ProductPage extends ConsumerStatefulWidget {
 }
 
 class _ProductPageState extends ConsumerState<ProductPage> {
-  final TextEditingController _controller = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    Future.microtask(() => ref.read(productsInitializationProvider));
-  }
-
   @override
   Widget build(BuildContext context) {
+    ref.watch(productsInitializationProvider);
+
     final products = ref.watch(productsNotifierProvider);
+    final productsAsyncValue = ref.watch(productsInitializationProvider);
 
     return HomeLayout(
       floatingButton: FloatingActionButton(
@@ -34,20 +32,41 @@ class _ProductPageState extends ConsumerState<ProductPage> {
         highlightElevation: 0,
         onPressed: () async {
           final newProduct = Product(
-              id: DateTime.now().millisecondsSinceEpoch,
-              name: 'Nuevo Producto',
-              price: 0,
-              description: 'Descripción del producto',
-              barcode: '',
-              status: false,
-              quantity: 10,
-              createdAt: DateTime.now(),
-              updatedAt: DateTime.now());
+            id: Random().nextInt(1000000),
+            name: getRandomString(10),
+            price: Random().nextDouble() * 100,
+            description: getRandomString(20),
+            barcode: getRandomString(6),
+            status: Random().nextBool(),
+            quantity: Random().nextInt(100),
+            createdAt:
+                DateTime.now().subtract(Duration(days: Random().nextInt(365))),
+            updatedAt:
+                DateTime.now().subtract(Duration(days: Random().nextInt(365))),
+          );
           ref.read(createProductProvider(newProduct).future);
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
-      children: Text('$products'),
+      children: productsAsyncValue.when(
+        data: (_) => Center(
+            child: products.isEmpty
+                ? const Text('No hay productos disponibles.')
+                : Column(
+                    children: products
+                        .map((product) => ListTile(
+                            title: Text(product.name),
+                            subtitle:
+                                Text(product.description ?? 'Sin descripción'),
+                            trailing: IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () => ref.read(
+                                    deleteProductProvider(product.id).future))))
+                        .toList(),
+                  )),
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (error, stack) => Center(child: Text('Error: $error')),
+      ),
     );
   }
 }
