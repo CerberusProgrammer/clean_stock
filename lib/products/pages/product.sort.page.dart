143d1125bd4b536899fad80939954ddeaab57992
Omar Flores <sazardev@gmail.com>
2024-07-02 16:08:13 -0700

    refactor: Update product page to improve sorting and filtering options

--- lib/products/pages/product.sort.page.dart
+++ lib/products/pages/product.sort.page.dart
@@ -1,3 +1,4 @@
+import 'package:clean_stock/components/custom_textbutton.dart';
 import 'package:clean_stock/components/selectable_listtile.dart';
 import 'package:clean_stock/models/ccategory.dart';
 import 'package:clean_stock/models/manufacturer.dart';
@@ -24,92 +25,100 @@ class ProductSortPage extends ConsumerWidget {
     final selectedManufacturers = ref.watch(selectedManufacturersProvider);
     final selectedSuppliers = ref.watch(selectedSuppliersProvider);
 
-    print(selectedCategories);
-
     return Scaffold(
-      body: ListView(
-        children: [
-          ExpansionTile(
-            title: const Text('Categories'),
-            children: categories.map((category) {
-              final isSelected = selectedCategories.contains(category);
+        body: ListView(
+          children: [
+            ExpansionTile(
+              title: const Text('Categories'),
+              children: categories.map((category) {
+                if (category == null) {
+                  return Container();
+                }
 
-              return SelectableListTile(
-                title: category?.name ?? 'none',
-                icon: Icons.category,
-                selected: isSelected,
-                onTap: (value) {
-                  if (isSelected) {
-                    ref.read(selectedCategoriesProvider.notifier).state =
-                        selectedCategories.where((c) => c != category).toList();
-                  } else {
-                    ref.read(selectedCategoriesProvider.notifier).state = [
-                      ...selectedCategories,
-                      category!,
-                    ];
-                  }
-                },
-              );
-            }).toList(),
-          ),
-          ExpansionTile(
-            title: const Text('Manufacturers'),
-            children: manufacturers.map((manufacturer) {
-              final isSelected = selectedManufacturers.contains(manufacturer);
-              return SelectableListTile(
-                title: manufacturer?.name ?? 'none',
-                icon: Icons.business,
-                selected: isSelected,
-                onTap: (value) {
-                  if (isSelected) {
-                    ref.read(selectedManufacturersProvider.notifier).state =
-                        selectedManufacturers
-                            .where((m) => m != manufacturer)
-                            .toList();
-                  } else {
-                    ref.read(selectedManufacturersProvider.notifier).state = [
-                      ...selectedManufacturers,
-                    ];
-                  }
-                },
-              );
-            }).toList(),
-          ),
-          ExpansionTile(
-            title: const Text('Suppliers'),
-            children: suppliers.map((supplier) {
-              final isSelected = selectedSuppliers.contains(supplier);
-              return SelectableListTile(
-                title: supplier?.name ?? 'none',
-                icon: Icons.supervisor_account,
-                selected: isSelected,
-                onTap: (value) {
-                  if (isSelected) {
-                    ref.read(selectedSuppliersProvider.notifier).state =
-                        selectedSuppliers.where((s) => s != supplier).toList();
-                  } else {
-                    ref.read(selectedSuppliersProvider.notifier).state = [
-                      ...selectedSuppliers,
-                    ];
-                  }
-                },
-              );
-            }).toList(),
-          ),
-        ],
-      ),
-      floatingActionButton: Row(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              ref.read(selectedCategoriesProvider.notifier).state = [];
-              ref.read(selectedManufacturersProvider.notifier).state = [];
-              ref.read(selectedSuppliersProvider.notifier).state = [];
-            },
-            child: const Text('Limpiar Filtros'),
-          )
-        ],
-      ),
-    );
+                final isSelected = selectedCategories.contains(category);
+
+                return SelectableListTile(
+                  title: category.name,
+                  icon: Icons.category,
+                  selected: isSelected,
+                  onTap: (value) {
+                    if (isSelected) {
+                      ref.read(selectedCategoriesProvider.notifier).update(
+                          (state) =>
+                              state.where((c) => c != category).toList());
+                    } else {
+                      ref
+                          .read(selectedCategoriesProvider.notifier)
+                          .update((state) => [...state, category]);
+                    }
+                  },
+                );
+              }).toList(),
+            ),
+            ExpansionTile(
+              title: const Text('Manufacturers'),
+              children: manufacturers.map((manufacturer) {
+                final isSelected = selectedManufacturers.contains(manufacturer);
+                if (manufacturer == null) {
+                  return Container();
+                }
+
+                return SelectableListTile(
+                  title: manufacturer.name,
+                  icon: Icons.business,
+                  selected: isSelected,
+                  onTap: (value) {
+                    if (isSelected) {
+                      ref.read(selectedManufacturersProvider.notifier).state =
+                          selectedManufacturers
+                              .where((m) => m != manufacturer)
+                              .toList();
+                    } else {
+                      ref.read(selectedManufacturersProvider.notifier).state = [
+                        ...selectedManufacturers,
+                      ];
+                    }
+                  },
+                );
+              }).toList(),
+            ),
+            ExpansionTile(
+              title: const Text('Suppliers'),
+              children: suppliers.map((supplier) {
+                final isSelected = selectedSuppliers.contains(supplier);
+
+                if (supplier == null) {
+                  return Container();
+                }
+
+                return SelectableListTile(
+                  title: supplier.name,
+                  icon: Icons.supervisor_account,
+                  selected: isSelected,
+                  onTap: (value) {
+                    if (isSelected) {
+                      ref.read(selectedSuppliersProvider.notifier).state =
+                          selectedSuppliers
+                              .where((s) => s != supplier)
+                              .toList();
+                    } else {
+                      ref.read(selectedSuppliersProvider.notifier).state = [
+                        ...selectedSuppliers,
+                      ];
+                    }
+                  },
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+        floatingActionButton: CustomTextButton(
+          onTap: () {
+            ref.read(selectedCategoriesProvider.notifier).state = [];
+            ref.read(selectedManufacturersProvider.notifier).state = [];
+            ref.read(selectedSuppliersProvider.notifier).state = [];
+          },
+          title: 'Limpiar Filtros',
+        ));
   }
 }
