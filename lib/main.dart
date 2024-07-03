fbc6e579254853d6903eaac0eafbde8a78f3cbe2
Omar Flores <sazardev@gmail.com>
2024-07-03 07:25:30 -0700

    refactor: Update main.dart to initialize Hive based on platform

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:clean_stock/models/ccategory.dart';
 import 'package:clean_stock/models/manufacturer.dart';
 import 'package:clean_stock/models/supplier.dart';
 import 'package:clean_stock/products/models/product.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,9 +13,14 @@ import 'package:path_provider/path_provider.dart' as path_provider;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final appDocumentDirectory =
-      await path_provider.getApplicationDocumentsDirectory();
-  await Hive.initFlutter(appDocumentDirectory.path);
+
+  if (!kIsWeb) {
+    final appDocumentDirectory =
+        await path_provider.getApplicationDocumentsDirectory();
+    await Hive.initFlutter(appDocumentDirectory.path);
+  } else {
+    await Hive.initFlutter();
+  }
 
   Hive.registerAdapter(ManufacturerAdapter());
   Hive.registerAdapter(ProductAdapter());
