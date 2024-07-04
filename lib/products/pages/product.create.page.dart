48cc0052120da182615d48a128c47082d373642e
Omar Flores <sazardev@gmail.com>
2024-07-03 17:05:43 -0700

    refactor: Add color selection feature to ProductCreatePage

--- lib/products/pages/product.create.page.dart
+++ lib/products/pages/product.create.page.dart
@@ -1,3 +1,6 @@
+import 'package:clean_stock/components/clean_card.dart';
+import 'package:clean_stock/components/clean_iconbutton.dart';
+import 'package:clean_stock/components/clean_textfield.dart';
 import 'package:clean_stock/components/custom_floatingbutton.dart';
 import 'package:clean_stock/layouts/home.layout.dart';
 import 'package:clean_stock/products/product.riverpod.dart';
@@ -5,15 +8,35 @@ import 'package:clean_stock/utils/generate_random_product.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProductCreatePage extends ConsumerWidget {
+class ProductCreatePage extends ConsumerStatefulWidget {
   const ProductCreatePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _ProductCreatePageState();
+}
+
+class _ProductCreatePageState extends ConsumerState<ProductCreatePage> {
+  Color? selectedColor;
+
+  final TextEditingController nameController = TextEditingController();
+
+  final colorsOptions = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.yellow,
+    Colors.purple,
+    Colors.orange,
+    Colors.pink,
+    Colors.teal,
+    Colors.cyan,
+    Colors.brown,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
     return HomeLayout(
-      children: Column(
-        children: [],
-      ),
       floatingButton: CustomFloatingButton(
         onPressed: () {
           final newProduct = generateRandomProduct();
@@ -21,6 +44,100 @@ class ProductCreatePage extends ConsumerWidget {
           Navigator.of(context).pop();
         },
         icon: Icons.add,
+      ),
+      children: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CleanIconButton(
+                color: selectedColor,
+                onTap: () => showModalBottomSheet(
+                  context: context,
+                  builder: (builder) => ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                    child: Scaffold(
+                      appBar: AppBar(
+                        title: const Text('Select a color'),
+                        automaticallyImplyLeading: false,
+                      ),
+                      body: GridView.count(
+                        padding: const EdgeInsets.all(16),
+                        crossAxisCount: 4,
+                        mainAxisSpacing: 8,
+                        crossAxisSpacing: 8,
+                        children: [
+                          ...colorsOptions.map(
+                            (color) => CleanIconButton(
+                              color: color,
+                              onTap: () {
+                                Navigator.of(context).pop();
+                                setState(() {
+                                  selectedColor = color;
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: CleanCard(
+                  color: selectedColor,
+                  child: Column(
+                    children: [
+                      Text(
+                        nameController.text.isEmpty
+                            ? 'Product name'
+                            : nameController.text.length > 7
+                                ? '${nameController.text.substring(0, 6)}...'
+                                : nameController.text,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      const Spacer(),
+                      SizedBox(
+                        height: 32,
+                        child: CleanCard(
+                          color: selectedColor,
+                          padding: null,
+                          child: const Center(
+                            child: Text(
+                              'MXN \$00.00',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: selectedColor?.withOpacity(0.1) ??
+                  Theme.of(context).colorScheme.primary.withOpacity(0.1),
+            ),
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                CleanTextField(
+                  labelText: 'Product name',
+                  controller: nameController,
+                  selectedColor: selectedColor,
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
