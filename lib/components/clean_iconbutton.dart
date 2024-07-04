49cb47986bfc14cb13e5e811d23013a26550b8c9
Omar Flores <sazardev@gmail.com>
2024-07-03 17:05:36 -0700

    refactor: Add CleanIconButton component for displaying clean icon buttons

---
+++ lib/components/clean_iconbutton.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CleanIconButton extends StatefulWidget {
+  const CleanIconButton({
+    super.key,
+    this.color,
+    this.onTap,
+    this.isExpanded = false,
+    this.size = const Size(40, 40),
+  });
+
+  final Color? color;
+  final Function()? onTap;
+  final bool isExpanded;
+  final Size size;
+
+  @override
+  State<CleanIconButton> createState() => _CleanIconButtonState();
+}
+
+class _CleanIconButtonState extends State<CleanIconButton> {
+  @override
+  Widget build(BuildContext context) {
+    Color effectiveColor =
+        widget.color ?? Theme.of(context).colorScheme.primary;
+
+    return InkWell(
+      onTap: widget.onTap,
+      borderRadius: BorderRadius.circular(10),
+      splashColor: effectiveColor.withOpacity(0.1),
+      focusColor: effectiveColor.withOpacity(0.1),
+      highlightColor: effectiveColor.withOpacity(0.1),
+      hoverColor: effectiveColor.withOpacity(0.1),
+      child: Container(
+        height: widget.size.height,
+        width: widget.size.width,
+        decoration: BoxDecoration(
+          color: effectiveColor.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Icon(
+          Icons.palette,
+          color: effectiveColor.withOpacity(0.6),
+        ),
+      ),
+    );
+  }
+}
