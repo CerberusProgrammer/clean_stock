b53411c4dea2ed0dbacd967f9248a6dd8c8de775
Omar Flores <sazardev@gmail.com>
2024-07-04 17:27:47 -0700

    refactor: Add CleanDropdownMenu component for dropdown menu functionality

---
+++ lib/components/clean_dropdownmenu.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CleanDropdownMenu<T> extends StatelessWidget {
+  const CleanDropdownMenu({
+    super.key,
+    required this.entries,
+    this.hintText,
+    this.helperText,
+    this.color,
+  });
+
+  final List<DropdownMenuEntry<T>> entries;
+  final String? hintText;
+  final String? helperText;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    Color effectiveColor = color ?? Theme.of(context).colorScheme.primary;
+
+    return DropdownMenu(
+      enableSearch: true,
+      hintText: hintText,
+      helperText: helperText,
+      dropdownMenuEntries: entries,
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide:
+              BorderSide(color: effectiveColor.withOpacity(0.6), width: 20),
+          gapPadding: 8,
+        ),
+      ),
+    );
+  }
+}
