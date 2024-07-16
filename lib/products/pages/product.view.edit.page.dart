df5d0b0b73eda2c554258bf3bf6901eff5b6442d
Omar Flores <cerberusprogrammer@gmail.com>
2024-07-15 20:48:09 -0700

    feat: Add ProductViewEditPage for editing product details

---
+++ lib/products/pages/product.view.edit.page.dart
@@ -0,0 +1,80 @@
+import 'package:clean_stock/components/custom_floatingbutton.dart';
+import 'package:clean_stock/layouts/home.layout.dart';
+import 'package:clean_stock/products/models/product.dart';
+import 'package:clean_stock/products/product.riverpod.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProductViewEditPage extends ConsumerStatefulWidget {
+  const ProductViewEditPage({super.key, required this.productId});
+  final String? productId;
+
+  @override
+  createState() => _ProductViewEditPageState();
+}
+
+class _ProductViewEditPageState extends ConsumerState<ProductViewEditPage> {
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController _nameController;
+
+  @override
+  void initState() {
+    super.initState();
+    final product = ref.read(productByIdProvider(widget.productId!));
+    _nameController = TextEditingController(text: product?.name ?? '');
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _updateProduct() async {
+    if (_formKey.currentState!.validate()) {
+      final updatedProduct = Product(
+        id: widget.productId!,
+        name: _nameController.text,
+        barcode: '',
+        status: false,
+        price: 0,
+        quantity: 0,
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+      );
+
+      ref.read(updateProductProvider(updatedProduct).future);
+
+      Navigator.of(context).pop();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return HomeLayout(
+      titleLastPath: 'Edit Product',
+      floatingButton: CustomFloatingButton(
+        onPressed: _updateProduct,
+        icon: Icons.done,
+      ),
+      children: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _nameController,
+              decoration: const InputDecoration(labelText: 'Product Name'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a product name';
+                }
+                return null;
+              },
+            ),
+            // Añade más campos aquí
+          ],
+        ),
+      ),
+    );
+  }
+}
