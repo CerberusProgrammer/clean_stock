135a633069c2e11253dffb0a603aaa66156dfed3
Omar Flores <cerberusprogrammer@gmail.com>
2024-07-15 20:47:57 -0700

    feat: Add edit button to ProductViewPage for editing product details

--- lib/products/pages/product.view.page.dart
+++ lib/products/pages/product.view.page.dart
@@ -1,7 +1,9 @@
+import 'package:clean_stock/components/custom_floatingbutton.dart';
 import 'package:clean_stock/layouts/home.layout.dart';
 import 'package:clean_stock/products/product.riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class ProductViewPage extends ConsumerWidget {
   const ProductViewPage({super.key, required this.productId});
@@ -30,12 +32,15 @@ class ProductViewPage extends ConsumerWidget {
 
     return HomeLayout(
       titleLastPath: product.name,
+      floatingButton: CustomFloatingButton(
+        onPressed: () => context.go('/products/$productId/edit'),
+        icon: Icons.edit,
+      ),
       children: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Product ID: ${product.id}'),
             Text('Product Name: ${product.name}'),
             Text('Product Price: ${product.price}'),
             Text('Product Description: ${product.description}'),
