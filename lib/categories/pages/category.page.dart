5d1ef6c1ce056065202795a9cbae2d08e36ed4a4
sazarcode <cerberusprogrammer@gmail.com>
2024-07-27 11:12:25 -0700

    refactor: Create CategoryPage widget for displaying category details

---
+++ lib/categories/pages/category.page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CategoryPage extends ConsumerWidget {
+  const CategoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
